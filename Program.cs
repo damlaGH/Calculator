01754c5c29de314c1cd21ebf6005b1e7bee5a5d9
Damla hanım <damlanurozturk@gmail.com>
2023-01-16 17:13:22 +0300

    added Color

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿namespace Calculator
-{ 
+{
     public class Program
     {
         public static void Main()
@@ -10,12 +10,12 @@
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("Menu\n1.Sum\n2.Subs\n3.Multi\n4.Divide\n5.Exit");
+            MessageColor("Menu\n1.Sum\n2.Subs\n3.Multi\n4.Divide\n5.Exit", ConsoleColor.Yellow);
             Choose();
         }
         static void Choose()
         {
-            Console.Write("Please choose which action do you want: ");
+            MessageColor("Please choose which action do you want: ",ConsoleColor.DarkCyan);
             string chosen = Console.ReadLine();
 
             switch (chosen)
@@ -43,60 +43,65 @@
         }
         static (double n1, double n2) takeNumbers()
         {
-            Console.WriteLine("please write number 1:");
+            MessageColor("please write number 1:", ConsoleColor.Magenta);
             double n1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("please write number 2:");
+            MessageColor("please write number 2:",ConsoleColor.DarkRed);
             double n2 = Convert.ToDouble(Console.ReadLine());
 
             return (n1, n2);
         }
-
         static void Sum()
-            {   var operants = takeNumbers();
-                double result = operants.n1 + operants.n2;
-                Console.WriteLine($"result: {result}");
-            }
+        {
+            var operants = takeNumbers();
+            double result = operants.n1 + operants.n2;
+            Console.WriteLine($"result: {result}");
+        }
         static void Subs()
-            {
-                var operants = takeNumbers();
-                double result = operants.n1 - operants.n2;
-                Console.WriteLine($"result: {result}");
-            }
+        {
+            var operants = takeNumbers();
+            double result = operants.n1 - operants.n2;
+            Console.WriteLine($"result: {result}");
+        }
         static void Multi()
-            {
-                var operants = takeNumbers();
-                double result = operants.n1 * operants.n2;
-                Console.WriteLine($"result: {result}");
-            }
+        {
+            var operants = takeNumbers();
+            double result = operants.n1 * operants.n2;
+            MessageColor($"result: {result}",ConsoleColor.Green);
+        }
         static void Divide()
-            {
-                var operants = takeNumbers();
-                double result = operants.n1 / operants.n2;
-                Console.WriteLine($"result: {result}");
-            }
+        {
+            var operants = takeNumbers();
+            double result = operants.n1 / operants.n2;
+            MessageColor($"result: {result}", ConsoleColor.Blue);
+        }
         static void Again()
+        {
+            Console.WriteLine("Do you want to make action again?:(Y/N)");
+            string again = Console.ReadLine();
+            switch (again)
             {
-                Console.WriteLine("Do you want to make action again?:(Y/N)");
-                string again = Console.ReadLine();
-                switch (again)
-                {
-                    case "Y":
-                    case "y":
-                        Menu();
-                        break;
-                    case "N":
-                    case "n":
-                        Environment.Exit(0);
-                        break;
+                case "Y":
+                case "y":
+                    Menu();
+                    break;
+                case "N":
+                case "n":
+                    Environment.Exit(0);
+                    break;
 
                 default:
                     Console.WriteLine("Chosen is not valid!");
                     Again();
                     break;
-                }
             }
         }
-  
+        static void MessageColor(string message, ConsoleColor messageColor)
+        {
+            Console.ForegroundColor = messageColor;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
     }
+}
 
 
